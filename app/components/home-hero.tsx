5243fc758a2120696fcc62bea5622b7b3dde9b77
CaiooFerreiaa <caioferreiraadev@gmail.com>
2026-07-07 21:49:59 -0300

    feat: refactor layout and add new components for improved structure

---
+++ app/components/home-hero.tsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <div>Hero
+      <div>
+        <span>Search</span>
+        <span><i>ico</i>Sequencia</span>
+        <span>Alertas</span>
+        <span>Perfil</span>
+      </div>
+
+      <div>
+        <div>Card de entrada</div>
+        <div>metas do dia</div>
+      </div>
+    </div>
+  )
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+    <div>
+      Ola, aqui é a página do usuário
+    </div>
+  )
+}

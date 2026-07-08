a529c6adbc66068341f98a644a2c8745ff15cd47
CaiooFerreiaa <caioferreiraadev@gmail.com>
2026-07-08 20:43:03 -0300

    feat: update layout of Hero component for improved structure and alignment

--- components/home-hero.tsx
+++ components/home-hero.tsx
@@ -7,21 +7,32 @@ export default function Hero() {
         <div className="w-[700px]">
           <InputSearch/>
         </div>
-        <div className="flex items-center justify-center">
-          <span className="material-symbols-outlined !text-[48px] !bg-gradient-to-r from-red-500 to-orange-600 bg-clip-text text-transparent">
-            local_fire_department
-          </span>
-          <div className="flex flex-col">
-            <span>
-              12
+        <div className="flex flex-row items-center gap-10">
+          <div className="flex items-center gap-3">
+            <span className="material-symbols-outlined !text-[48px] !bg-gradient-to-r from-red-500 to-orange-600 bg-clip-text text-transparent">
+              local_fire_department
+            </span>
+            <div className="flex flex-col">
+              <span>
+                12
+              </span>
+              <span>
+                Dias de sequência
+              </span>
+            </div>
+          </div>
+          <div>
+            <span className="material-symbols-outlined !text-[36px]">
+              notifications
             </span>
             <span>
-              Dias de sequência
+              1
             </span>
           </div>
+          <div>
+            <img src="#" alt="Foto de Perfil" />
+          </div>
         </div>
-        <div>Alertas</div>
-        <div>Perfil</div>
       </div>
 
       <div className="flex flex-row">
